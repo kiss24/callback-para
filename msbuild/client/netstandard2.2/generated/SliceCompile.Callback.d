3aa2753c74a1b7aeb38c0efc5f001b59532737e6
zp <y.fox@qq.com>
2019-08-01 16:46:50 +0800

    update.

---
+++ msbuild/client/netstandard2.2/generated/SliceCompile.Callback.d
@@ -0,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<dependencies>
+  <source name="../../../Callback.ice">
+    <options>
+      <IceHome>/home/zp/.nuget/packages/zeroc.ice.net/3.7.2/build/..</IceHome>
+      <IceToolsPath>/usr/bin</IceToolsPath>
+      <OutputDir>/home/zp/Documents/Ice-csharp/callback-para/msbuild/client/netstandard2.2/generated</OutputDir>
+    </options>
+  </source>
+</dependencies>
